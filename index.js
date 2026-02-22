d1d776cee0e3a05987f70921489554c3940ba963
gardin32 <gardin32@msu.edu>
2026-02-22 17:27:03 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ ScrollReveal().reveal('img[alt="Parents"]', {
   duration: 700,
   origin: 'bottom'
 });
+
+ScrollReveal().reveal('img[alt="Siblings"]', {
+  distance: '40px',
+  duration: 700,
+  origin: 'left'
+});
+
+ScrollReveal().reveal('img[alt="Family1"]', {
+  distance: '40px',
+  duration: 700,
+  origin: 'right'
+});
+
+ScrollReveal().reveal('img[alt="Family2"]', {
+  distance: '40px',
+  duration: 700,
+  origin: 'top'
+});
+
+ScrollReveal().reveal('img[alt="Family3"]', {
+  distance: '40px',
+  duration: 700,
+  origin: 'bottom'
+});
